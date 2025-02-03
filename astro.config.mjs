a9b5031ffff7facbfc5fb3b9bc9864c5be9d58b1
Vladislav Zernov <zernovvladislav7@gmail.com>
2025-02-03 19:26:02 +0100

    added base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: "https://zervladpy.github.io",
+  base: "/portfolio/",
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en", "ru", "pt", "it"]
