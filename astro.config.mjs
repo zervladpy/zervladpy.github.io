3a4c08bcc773ad70453f25542fa305275a154413
Vladislav Zernov <zernovvladislav7@gmail.com>
2025-02-03 19:17:15 +0100

    added astro workflow to deploy in github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://zervladpy.github.io",
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en", "ru", "pt", "it"]
